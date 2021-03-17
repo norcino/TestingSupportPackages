e9a00c691ceec46f82d370a18d12c13f743218c5
Manuel Salvatori <manuel.salvatori@gmail.com>
2021-03-17 21:37:39 +0000

    Fixed issues with fluent assertion committed working copy with green tests.

---
+++ FluentAssertion.MSTest/Framework/AssertObjectInCollection.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System;
+
+namespace FluentAssertion.MSTest.Framework
+{
+    public class AssertObjectInCollection<T>
+    {
+        public readonly ReadOnlyCollection<T> Collection;
+        public readonly T Element;
+
+        public AssertObjectInCollection(AssertCollection<T> enumerable, T element)
+        {
+            Collection = new ReadOnlyCollection<T>(enumerable.Collection);
+            Element = element;
+        }
+    }
+}
---
+++ FluentAssertion.MSTest/Framework/AssertObjectProperty.cs
@@ -0,0 +1,14 @@
+﻿namespace FluentAssertion.MSTest.Framework
+{
+    public class AssertObjectProperty<T,P>
+    {
+        public readonly AssertObject<T> AssertObject;
+        public readonly P Property;
+
+        public AssertObjectProperty(AssertObject<T> assertObject, P property)
+        {
+            AssertObject = assertObject;
+            Property = property;
+        }
+    }
+}

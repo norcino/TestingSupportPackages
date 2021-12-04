4c9260d5b906a731ee4e2f3da6526b38ab947969
Manuel Salvatori <manuel.salvatori@gmail.com>
2021-12-04 15:59:17 +0000

    Rename ToList in AsList

--- Builder/ExtensionMethods.cs
+++ Builder/ExtensionMethods.cs
@@ -11,7 +11,7 @@ namespace Builder
         /// <typeparam name="TE">Type of the object</typeparam>
         /// <param name="singleObject">Instance of the object</param>
         /// <returns>List containing only the given object</returns>
-        public static List<TE> ToList<TE>(this TE singleObject)
+        public static List<TE> AsList<TE>(this TE singleObject)
         {
             return new List<TE> { singleObject };
         }

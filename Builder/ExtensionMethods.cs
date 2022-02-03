77d49a333950018567b978057d7cbba429f9522d
Manuel Salvatori <manuel.salvatori@gmail.com>
2022-02-03 18:25:39 +0000

    Renamed extension method from AsQueryable in ToQueryable to avoid clashes with linq methods

--- Builder/ExtensionMethods.cs
+++ Builder/ExtensionMethods.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Builder
@@ -12,7 +13,7 @@ namespace Builder
         /// <param name="singleObject">Instance of the object</param>
         /// <returns>List containing only the given object</returns>
         public static List<TE> AsList<TE>(this TE singleObject)
-        {
+        {            
             return new List<TE> { singleObject };
         }
 
@@ -22,9 +23,9 @@ namespace Builder
         /// <typeparam name="TE">Type of the object</typeparam>
         /// <param name="singleObject">Instance of the object</param>
         /// <returns>IQueryable containing the given object</returns>
-        public static IQueryable<TE> AsQueryable<TE>(this TE singleObject)
+        public static IQueryable<TE> ToQueryable<TE>(this TE singleObject)
         {
             return new List<TE> { singleObject }.AsQueryable<TE>();
-        }
+        }    
     }
 }
